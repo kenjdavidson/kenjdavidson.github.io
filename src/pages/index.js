0f6d0b4f8526cd364bc8956d1f8c98402b522de6
Ken Davidson <ken.j.davidson@live.ca>
2020-08-05 11:49:32 -0400

    Update index.js
    Removed covid comment/

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,11 +25,6 @@ export default ({ data }) => {
           my <Link to="/resume">professional experience</Link> and <Link to="/about">personal interests</Link>.
         </div>
       </PageHeader>
-      <blockquote class="error">
-        <p>
-          Everyone stay safe in these crazy <strong>COVID-19</strong> times!!  Stay home when you can or are feeling ill, if you get out for exercise or shopping respect others personal space, and just try to be patient!  This is going on, it's going to go on for a while, the only way we get through this is supporting each other (and the government, regardless of your beliefs).
-        </p>
-      </blockquote>
       <Section>
         <SectionHeader>Recent</SectionHeader>
         <ArticleGrid posts={ data.allMdx.edges }></ArticleGrid>                    

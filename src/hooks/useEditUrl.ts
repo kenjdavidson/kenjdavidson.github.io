d8b56f359ba555860f49cde60adff676b2fe15d0
Ken Davidson <ken.j.davidson@live.ca>
2021-05-13 10:23:11 -0400

    (fix): updated edit link hook

--- src/hooks/useEditUrl.ts
+++ src/hooks/useEditUrl.ts
@@ -2,11 +2,10 @@ import { useStaticQuery, graphql } from "gatsby";
 import { SiteMetadata } from "../graphql/siteMetadata";
 import useSiteMetadata from "./useSiteMetadata";
 
-const project = "kenjdavidson.gatsby.io";
 const useEditUrl: (filepath: string) => string = filepath => {
   const { editUrl } = useSiteMetadata();
-  const index = filepath.indexOf(project);
-  return `${editUrl}/${filepath.substring(index + project.length + 1)}`;
+  const index = filepath.indexOf(`/content`);
+  return `${editUrl}/${filepath.substring(index)}`;
 };
 
 export default useEditUrl;

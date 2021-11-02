eea63b2e6d8e94bb1765ac4aa805434b7f05033d
kendavidson <ken.j.davidson@live.ca>
2021-11-02 10:05:37 -0400

    Updated gatsby and items

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,24 @@
+/** @jsx jsx */
 import * as React from "react"
 import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
+import Title from "@lekoarts/gatsby-theme-minimal-blog/src/components/title"
+import { jsx, Box, Heading } from 'theme-ui'
+import { Link } from 'gatsby'
 
-const NotFound = () => (
-  <Layout>
-    <h1>This don't look like the <i>Co Ach Ella Valley</i> !</h1>
-    <p>Looks like you should have taken a left turn at Albuquerque!</p>
+const NotFound = () => (  
+  <Layout>    
+    <Heading as="h1" variant="styles.h1">
+      Welp...
+    </Heading>
+    <section sx={{ my: 5, mb: [5,5,6], variant: `layout.content`, p: { fontSize: [1, 2, 3], mt: 2 } }}>      
+        <p>Looks like you should have taken a left turn at Albuquerque! There isn't much (I know) so you've only got a couple options:</p>        
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/writing">Writing</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+    </section>
   </Layout>
 )
 
